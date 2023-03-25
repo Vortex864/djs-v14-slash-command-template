a4426d4b9efb7819028c6012008e41079b6512a5
Vortex864 <78692711+Vortex864@users.noreply.github.com>
2023-03-25 19:57:38 +0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('name') // Replace the name of your command to your command name.
+    .setDescription('description') // Replace the description of your command to your command description.
+    
+    async execute(interaction) {
+    
+    
+    }
+ };
